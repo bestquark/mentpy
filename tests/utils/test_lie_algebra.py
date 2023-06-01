2e9dd06d94469410805c0ddcf231084052c66118
Luis Mantilla <luismantilla99@outlook.com>
2023-06-01 16:33:30 -0700

    add test to cylinder lie alg

--- tests/utils/test_lie_algebra.py
+++ tests/utils/test_lie_algebra.py
@@ -6,11 +6,17 @@ import mentpy as mp
 def test_lie_algebra_grid(n_wires):
     gs = mp.templates.grid_cluster(n_wires, n_wires + 3)
     lieAlg = mp.utils.calculate_lie_algebra(gs, max_iter=100000)
-    assert len(lieAlg) == mp.utils.dim_su(2**n_wires) + 1
+    assert (
+        len(lieAlg) == mp.utils.dim_su(2**n_wires) + 1
+    ), f"Grid cluster of size ({n_wires}, {n_wires + 3}) failed"
 
 
-# @pytest.mark.parametrize("n_wires", [4, 5])
-# def test_lie_algebra_cylinder(n_wires):
-#     gs = mp.templates.grid_cluster(n_wires, n_wires + 3, periodic=True)
-#     lieAlg = mp.utils.calculate_lie_algebra(gs, max_iter=100000)
-#     assert len(lieAlg) == mp.utils.dim_so(2**n_wires) + 1
+@pytest.mark.parametrize("n_wires", [4])
+def test_lie_algebra_cylinder(n_wires):
+    gs = mp.templates.grid_cluster(n_wires, n_wires + 3, periodic=True)
+    extralegs = mp.templates.many_wires([2] * n_wires)
+    gs = mp.hstack((extralegs, gs, extralegs))
+    lieAlg = mp.utils.calculate_lie_algebra(gs, max_iter=100000)
+    assert (
+        len(lieAlg) == mp.utils.dim_so(2**n_wires) + 1
+    ), f"Cylinder cluster of size ({n_wires}, {n_wires + 2}) failed"
